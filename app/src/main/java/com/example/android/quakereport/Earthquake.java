ece0fb858b11260ebbd864dff93dc40495b2e49a
Paulo Henrique <p.henriquerm@hotmail.com>
2019-08-07 17:10:37 -0300

    Add method to format date and place here and change value of the third parameter of the constructor to Long

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,14 +1,20 @@
 package com.example.android.quakereport;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public class Earthquake {
     private double mMag;
     private String mPlace;
     private String mDate;
 
-    public Earthquake(double mMag, String mPlace, String mDate) {
-        this.mMag = mMag;
-        this.mPlace = mPlace;
-        this.mDate = mDate;
+    public Earthquake(double mag, String place, Long date) {
+        this.mMag = mag;
+        this.mPlace = setPlaceFormatted(place);
+        this.mDate = setDateFormatted(date);
+
     }
 
     public double getMag() {
@@ -22,4 +28,32 @@ public class Earthquake {
     public String getDate() {
         return mDate;
     }
+
+    private String setDateFormatted(Long timeInMilliseconds){
+        Date date = new Date(timeInMilliseconds);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM DD , YYYY");
+        return dateFormat.format(date);
+    }
+
+    private String setPlaceFormatted(String place){
+        List<String> list;
+        String placeFormatted;
+
+        //Formatted string place to better form of visualization
+        list = Arrays.asList(place.split(","));
+        int index = list.get(0).indexOf("of");
+        String city = list.get(0).substring((index + 3));
+
+        try{
+
+            String country = list.get(1);
+            placeFormatted = city + ", " + country;
+
+        }catch (ArrayIndexOutOfBoundsException e){
+            placeFormatted = city;
+        }
+
+        return placeFormatted;
+    }
+
 }
