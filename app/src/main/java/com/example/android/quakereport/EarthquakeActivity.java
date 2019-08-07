449906846e06bdff92c0fa94c85e23fa4fc97285
Paulo Henrique <p.henriquerm@hotmail.com>
2019-08-07 11:55:16 -0300

    Change the class to receive a list from an API instead of an array

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -23,25 +23,18 @@ import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
-    public static final String LOG_TAG = EarthquakeActivity.class.getName();
+//    private ArrayList<Earthquake> earthquakes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake(7.2, "San Francisco", "Feb 2, 2016"));
-        earthquakes.add(new Earthquake(6.1, "London", "July 20, 2015"));
-        earthquakes.add(new Earthquake(3.9, "Tokyo", "Nov 10, 2014"));
-        earthquakes.add(new Earthquake(5.4, "Mexico City", "May 3, 2014"));
-        earthquakes.add(new Earthquake(2.8, "Moscow", "Jan 3, 2013"));
-        earthquakes.add(new Earthquake(4.9, "Rio de Janeiro", "Aug 19, 2012"));
-        earthquakes.add(new Earthquake(1.6, "Paris", "Oct 30, 2011"));
-
+        ArrayList<Earthquake> earthquakes;
 
         ListView earthquakeListView = findViewById(R.id.list);
+
+        earthquakes = QueryUtils.extractEarthquakes();
 
 
         EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
